21acdeb4434d8d15fea0a65bbb80bd3e98618f6f
Zihan Guo <92571234+EllianaAkai@users.noreply.github.com>
2024-11-12 01:17:02 -0500

    update

---
+++ proposal.md
@@ -0,0 +1,18 @@
+## Zihan Guo 15-112 Term Project Proposal
+
+### Project Title: Café Poirot with Amuro Tooru
+### Project Description
+This project is inspired by *Detective Conan*. Amuro Tooru, an important character in *Detective Conan* runs a cafe called Café Poirot, also known as "喫茶ポアロ". The project "Café Poirot with Amuro Tooru" is a simulation game, where the player will be helping Amuro Tooru to run Café Poirot. The player starts off with the first day of work at Café Poirot, where there will be only a few menu options available. Throughout each workday, the player is able to move the character around, select ingredients, cook food, and serve dishes to customers by walking up to them. The customers will be characters from *Detecive Conan*. The customers will be randomly selected from a list of characters, all of whom are from *Detective Conan*, and a certain customer will be ensured that they will not be appearing more than once in the cafe. The customer's orders will also be randomly selected from a list of available menu items. After the player worked for a certain amount of days, new menu items will be unlocked. One workday consists of a successfully serving dishes to a certain number of customers.
+
+
+
+### Similar Projects
+
+Summer Sun - Undercooked
+Michelle Chen - Chaotic Cooking!
+
+
+### Version Control
+
+
+### Tech List
--- starter.py
+++ starter.py
@@ -5,12 +5,15 @@ from PIL import Image
 def onAppStart(app):
     # app.cafeURL = 'cmu://872707/34739461/cafeImage.PNG'
     # app.amuroURL = 'cmu://872707/34739463/amuroImage.png'
-    pass
+    print('hello')
 
 def redrawAll(app):
-    drawImage('./images/cafeEntireImage.PNG', 0, 0, width=640, height=640)
-    
+    drawImage('./images/cafeImage.PNG', 0, 0, width=640, height=640)
     drawImage('./images/amuroImage.PNG', amuro.playerPosX, amuro.playerPosY, width=128, height=128, align='center')
+    
+    addCafeCustomerDesks()
+    addCafeKitchenDesks()
+    addCafeChairs()
     
 def onKeyPress(app, key):
     amuro.playerKeyPress(key)
@@ -19,6 +22,26 @@ def onKeyPress(app, key):
 def onKeyHold(app, key):
     amuro.playerKeyPress(key)
     amuro.move()
+
+def addCafeCustomerDesks():
+    drawImage('./images/desk1Image.PNG', 96, 544, width=64, height=64, align='center')
+    drawImage('./images/desk1Image.PNG', 224, 544, width=64, height=64, align='center')
+    drawImage('./images/desk1Image.PNG', 352, 544, width=64, height=64, align='center')
+    drawImage('./images/desk1Image.PNG', 416, 544, width=64, height=64, align='center')
+
+def addCafeKitchenDesks():
+    drawImage('./images/desk2Image.PNG', 160, 224, width=64, height=64, align='center')
+    drawImage('./images/desk2Image.PNG', 160, 288, width=64, height=64, align='center')
+    drawImage('./images/desk2Image.PNG', 224, 288, width=64, height=64, align='center')
+    drawImage('./images/desk2Image.PNG', 288, 288, width=64, height=64, align='center')
+    drawImage('./images/desk2Image.PNG', 352, 288, width=64, height=64, align='center')
+    drawImage('./images/desk2Image.PNG', 416, 288, width=64, height=64, align='center')
+
+def addCafeChairs():
+    drawImage('./images/chairImage.PNG', 96, 480, width=64, height=64, align='center')
+    drawImage('./images/chairImage.PNG', 224, 480, width=64, height=64, align='center')
+    drawImage('./images/chairImage.PNG', 352, 480, width=64, height=64, align='center')
+    drawImage('./images/chairImage.PNG', 416, 480, width=64, height=64, align='center')
 
 def main():
     runApp(width=640, height=640)
